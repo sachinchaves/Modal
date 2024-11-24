e4162979fb85fae52145c61e3c9e3c76ac316a4d
sachinchaves <sachin.chaves1991@gmail.com>
2024-11-23 18:18:23 -0800

    add event listeners and rfactored code

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const buttons = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", openModal);
+}
+
+document.querySelector(".close-modal").addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
